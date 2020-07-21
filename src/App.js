9e7b930028bc98fe096612f8664bf468f2ac91bc
Nicolas Hanout <nicolas.hanout@gmail.com>
2020-07-21 13:48:21 -0700

    made a change on the feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey There.
+          I was Added on the feature branch
         </p>
         <a
           className="App-link"
